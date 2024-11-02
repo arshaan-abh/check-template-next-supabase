46907de0346e25cef786c5140386914366352d42
arshaan-abh <arshaan.abh@gmail.com>
2024-11-02 16:29:35 +0330

    Iconify checked

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
 import localFont from "next/font/local";
 import HeaderAuth from "@/components/header-auth";
 import DeployButton from "@/components/deploy-button";
+import MaterialSymbolsDoneRounded from "~icons/material-symbols/done-rounded.jsx";
 import "./globals.css";
 
 const geistSans = localFont({
@@ -51,6 +52,7 @@ export default function RootLayout({
               <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
                 <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
                   <div className="flex gap-5 items-center font-semibold">
+                    <MaterialSymbolsDoneRounded />
                     <Link href={"/"}>Next.js Supabase Starter</Link>
                     <div className="flex items-center gap-2">
                       <DeployButton />
